f2bc3168454c03cfd5f71e3b7349ec509f4d696c
Nuttapol Nonseeveerachai <nuttapol.nonsee@gmail.com>
2016-09-01 23:06:59 +0700

    Move style and script to seperated css and js files.

---
+++ resources/js/script.js
@@ -0,0 +1,30 @@
+var vue_todo = new Vue({
+	el:		'todo',
+	data:	{
+		newTodoText: '',
+		todoList: [
+			{ text: 'First todo.'		, done: true },
+			{ text: 'Second todo.' 	, done: false },
+			{ text: 'Third todo.' 	,	done: true },
+		]
+	},
+	methods: {				
+		// Add new todo to the list.
+		addTodo: function() {
+			var text = this.newTodoText.trim()
+			if (text) {
+				this.todoList.push({ text: text, done: false })
+				this.newTodoText = ''
+			}
+		},
+		// Toggle done.
+		toggleDoneTodo: function(index) {
+			this.todoList[index].done	=	!this.todoList[index].done
+
+		},
+		// Remove todo from todoList array.
+		removeTodo: function (index) {
+			this.todoList.splice(index, 1)
+		}
+	}
+})
